97b2e628dcefa2f67c9d53a96420094267ad9aa5
A. Bram Neijt <bneijt@gmail.com>
2012-09-07 20:16:34 +0200

    Use bash by default

--- config/runjob.sh
+++ config/runjob.sh
@@ -2,6 +2,6 @@
 /etc/init.d/mountall.sh
 /bin/rm -rf /job/zero
 cd /job
-PATH="/job/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" ./run.sh
+PATH="/job/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" /bin/bash run.sh
 /bin/dd if=/dev/zero of=/job/zero
 /sbin/poweroff
