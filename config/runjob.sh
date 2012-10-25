379058b093e1cf0fa290bbf9674400ef9d74fd38
A. Bram Neijt <bneijt@gmail.com>
2012-10-25 22:37:26 +0200

    Move to deamon based architecture with init script

--- config/runjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/etc/init.d/mountall.sh
-/bin/rm -rf /job/zero
-cd /job
-PATH="/job/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" /bin/bash run.sh
-/bin/dd if=/dev/zero of=/job/zero
-/sbin/poweroff
