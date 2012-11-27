c42d415f74d35e44b1b1fe240666d80759b99197
A. Bram Neijt <bneijt@gmail.com>
2012-11-27 09:42:51 +0100

    Scripts to create GHC installation job
    This job will create a tar which contains a
    GHC installation. The tar can be added
    to other jobs to install GHC quickly.

---
+++ jobs/ghc/README.md
@@ -0,0 +1,2 @@
+Create a tar file in the output directory containing
+the GHC installation in /opt
---
+++ jobs/ghc/ghcjob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd "`dirname "$0"`"
+set -e -x
+GHC_VERSION=7.6.1
+
+mkdir -p job/bin
+if [ ! -f "job/ghc-${GHC_VERSION}.tar.bz2" ]; then
+    wget "http://www.haskell.org/ghc/dist/${GHC_VERSION}/ghc-${GHC_VERSION}-x86_64-unknown-linux.tar.bz2" -O job/ghc-${GHC_VERSION}.tar.bz2
+fi
+
+sed -s 's/GHC_VERSION/'${GHC_VERSION}'/g' < construir > job/bin/construir
+chmod a+x job/bin/construir
+
+mkdir -p job/output
+../mkjob.py --extra-space 1000 job
+
